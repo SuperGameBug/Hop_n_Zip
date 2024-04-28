691bc9b04c5f782dd6a9752e648e15f98f2e8c9b
Sumit Das <48118135+SuperGameBug@users.noreply.github.com>
2024-04-28 16:46:13 +0530

    Changed Resolution

---
+++ systems/World/Spawners/WallSpawner/platform_spawner.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+var TrunkPlatform = preload("res://systems/World/TreeTrunk/obj_Tree_Trunk.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- systems/World/TreeTrunk/sct_Tree_Trunk.gd
+++ systems/World/TreeTrunk/sct_Tree_Trunk.gd
@@ -1,15 +1,12 @@
 extends Node2D
 
+class_name Tree_Trunk
+
 # Sprite variables
 var _trunkSprite : Sprite2D
-var _spriteMaterial : Material
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_trunkSprite = $TrunkSprite
-	_spriteMaterial = _trunkSprite.get_material()
-	
-	_spriteMaterial.set_shader_parameter("_frame", randi_range(0,1))
-	_spriteMaterial.set_shader_parameter("_spriteLength", 2.)
-	
-	
+	_trunkSprite.frame = randi_range(0,1)
+	_trunkSprite.flip_v = randi_range(0,1)
